cc643c898b739b85ce422be11446c0aac27524c2
rnzndrei <renzandreidelrosario@gmail.com>
2025-05-19 11:58:56 +0800

    Fixed Print in student

--- student/student.php
+++ student/student.php
@@ -77,9 +77,8 @@ $sql = "SELECT * FROM tblstudent order by fldindex";
 <td colspan="5" align=center>
     <button type="button" class="btn btn-warning btn-s" onClick="window.location.href='student.php'">Display All</button>
     <button type="button" class="btn btn-warning btn-s" onClick="window.location.href='insert.php'">Insert</button>
-    <button type="reset" class="btn btn-warning btn-s" onClick="window.location.href='../../tcpdf6/examples/aaarepstudent.php'">Print</button>
+    <button type="reset" class="btn btn-warning btn-s" onClick="window.location.href='../tcpdf6/examples/aaarepstudent.php'">Print</button>
     <button type="reset" class="btn btn-warning btn-s" onClick="window.location.href='../index.php'">Back</button>
-    <a href="tcpdf6/examples/aaarepstudent.php" target="_blank"> Print</a>
 </td>
 </tr>
 
