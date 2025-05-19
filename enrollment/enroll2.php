65a85e5324f0550e03f8bd914dcd4ee42e9b0f57
rnzndrei <renzandreidelrosario@gmail.com>
2025-05-19 11:26:28 +0800

    Duplicate course validation

--- enrollment/enroll2.php
+++ enrollment/enroll2.php
@@ -4,16 +4,12 @@ require("../include/conn.php");
 $vstudentindex=$_REQUEST['vid'];
 $vcourseindex=$_REQUEST['vid1'];
 $vstudentnumber=$_REQUEST['vid2'];
-$vcourseexist=$_REQUEST['vid'];
 
 $vindex=0;
 
 $vdupcourse=0;
 
-if($vstudentnumber==$vstudentnumberold)
-{
-    $vdupcourse=0;
-}
+
 
 $sql = "SELECT * FROM tbllist order by fldindex";
         $result = $conn->query($sql);
@@ -28,6 +24,19 @@ $sql = "SELECT * FROM tbllist order by fldindex";
         }
         $vindex=$vindex+1;
 
+$sql = "SELECT * FROM tbllist  WHERE fldcourseindex='$vcourseindex' order by fldindex";
+        $result = $conn->query($sql);
+        if($result->num_rows > 0) 
+        {
+            while($row = $result->fetch_assoc())
+            {
+                
+                $vdupcourse=1;		
+                
+            }
+        }
+
+if($vdupcourse ==  0){
     $sql="INSERT INTO tbllist (fldindex, fldstudentindex, fldcourseindex) VALUES ('$vindex', '$vstudentindex', '$vcourseindex')";
     if ($conn->query($sql) === TRUE) 
     {
@@ -40,5 +49,17 @@ $sql = "SELECT * FROM tbllist order by fldindex";
     <script>
         alert("Student Enrolled.");								
     </script>
-    <meta  http-equiv="refresh" content=".000001;url=enroll1.php?vid=<?php echo $vstudentnumber; ?>" />
+    <meta  http-equiv="refresh" content="0.00001;url=enroll1.php?vid=<?php echo $vstudentnumber; ?>" />
     <?php
+}else{
+    ?>
+    <script>
+    alert("You already enrolled in this course.");								
+    </script>
+    <meta  http-equiv="refresh" content="0.00001;url=enroll1.php?vid=<?php echo $vstudentnumber; ?>" />
+    <?php
+}
+?>
+
+
+
